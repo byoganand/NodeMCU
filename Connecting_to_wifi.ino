9e3ebfad5913bfa6f33acd5e9fb157793b4a00bf
Yoganand Biradavolu <52965130+byoganand@users.noreply.github.com>
2019-12-02 23:25:13 +0530

    Add files via upload

---
+++ Connecting_to_wifi.ino
@@ -0,0 +1,29 @@
+#include <ESP8266WiFi.h>
+
+const char *ssid =  "Yoganand";
+const char *pass =  "yogi0102";
+
+WiFiClient client;
+ 
+void setup() 
+{
+       Serial.begin(9600);
+       delay(10);
+               
+       Serial.println("Connecting to ");
+       Serial.println(ssid); 
+ 
+       WiFi.begin(ssid, pass); 
+       while (WiFi.status() != WL_CONNECTED) 
+          {
+            delay(500);
+            Serial.print(".");
+          }
+      Serial.println("");
+      Serial.println("WiFi connected"); 
+}
+ 
+void loop() 
+{      
+  
+}
